1e50a3c300f5a984ca3cf6f6b7b439c3029ee2a3
Robbie McArthur <robbiemca123@gmail.com>
2025-02-06 20:07:20 +0000

    Update allowed origins in stompconfig and remove unused dependencies

--- src/main/java/org/robbiemcarthur/whowantstorunstandup/configuration/WebSocketConfig.java
+++ src/main/java/org/robbiemcarthur/whowantstorunstandup/configuration/WebSocketConfig.java
@@ -17,6 +17,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/game").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/game")
+                .setAllowedOrigins("https://who-wants-to-run-stand-up.netlify.app", "http://localhost:3000")
+                .withSockJS();
     }
 }
