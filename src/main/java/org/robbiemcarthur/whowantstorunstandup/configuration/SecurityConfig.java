37d7b985aa404bdfe4296100962156035ccc6674
Robbie McArthur <robbiemca123@gmail.com>
2025-02-06 19:29:37 +0000

    Add allowed origin for netlify deployed frontend

--- src/main/java/org/robbiemcarthur/whowantstorunstandup/configuration/SecurityConfig.java
+++ src/main/java/org/robbiemcarthur/whowantstorunstandup/configuration/SecurityConfig.java
@@ -10,6 +10,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.List;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -25,7 +27,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.setAllowedOrigins(List.of("https://who-wants-to-run-stand-up-svc-d9d35b0792e6.herokuapp.com","http://localhost:3000"));
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
         configuration.setAllowCredentials(true);
